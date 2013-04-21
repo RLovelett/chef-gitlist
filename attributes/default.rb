5b761667d754da8f59f43ad5897b9d62136c65d2
Ryan Lovelett <ryan@lovelett.me>
2013-04-20 23:55:39 -0500

    Configure Xdebug

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,6 @@
 # Prevent the default site from starting
 default['apache']['default_site_enabled'] = false
+
+# Xdebug configuration options
+default['gitlist']['xdebug_remote_host'] = '192.168.50.1'
+default['gitlist']['xdebug_remote_port'] = 9000
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,17 +6,40 @@
 #
 # All rights reserved - Do Not Redistribute
 
+include_recipe 'git'
 include_recipe 'apache2'
 include_recipe 'apache2::mod_php5'
 include_recipe 'apache2::mod_deflate'
 include_recipe 'apache2::mod_rewrite'
 include_recipe 'php'
 include_recipe 'composer'
-include_recipe 'pdepend::composer'
+include_recipe 'build-essential'
 include_recipe 'phpunit'
-include_recipe 'phpcpd'
-include_recipe 'phploc'
-include_recipe 'phpmd::composer'
+#include_recipe 'pdepend::composer'
+#include_recipe 'phpcpd'
+#include_recipe 'phploc'
+#include_recipe 'phpmd::composer'
+
+php_pear_channel 'pecl.php.net' do
+  action :update
+end
+
+php_pear 'xdebug' do
+  # Specify that xdebug.so must be loaded as a zend extension
+  zend_extensions ['xdebug.so']
+  action :install
+end
+
+template '/etc/php5/conf.d/xdebug.ini' do
+  source 'xdebug.ini.erb'
+  mode 0644
+  owner 'root'
+  group 'root'
+  variables({
+    :remote_host => node['gitlist']['xdebug_remote_host'],
+    :remote_port => node['gitlist']['xdebug_remote_port']
+  })
+end
 
 link '/var/www/gitlist' do
   to '/vagrant'
